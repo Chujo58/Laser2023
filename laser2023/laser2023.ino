d6334bf54a63ed620b7b86bdf3240345353d6b7a
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2023-08-17 16:55:50 -0400

    Weird compilation problems - Removing the Lib

--- laser2023/laser2023.ino
+++ laser2023/laser2023.ino
@@ -1,5 +1,4 @@
-#include "Lib/Adafruit_Motor_Shield_V2_Library/Adafruit_MotorShield.h"
-// #include "Adafruit_MotorShield.h"
+#include "Adafruit_MotorShield.h"
 #define BAUD_RATE 115200
 
 // #define ADDRESS 0x62
@@ -13,10 +12,10 @@ unsigned delays = 20;
 unsigned value = 0;
 
 // Here we call the motor shield which will connect us to the stepper motor connected.
-auto AFMS = Adafruit_MotorShield();
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Here we connect to the stepper motor and we specify that we are using 200 steps per revolution.
-// Adafruit_StepperMotor * myMotor = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor * myMotor = AFMS.getStepper(200, 1);
 
 void setup(){
     // To communicate with Python we will do so via the Serial monitor. So we will first initialize it.
@@ -24,7 +23,7 @@ void setup(){
     Serial.println("LASER 2023");
 
     // Let's start the motor shield with the default frequency of 1.6kHz. The frequency can always be changed at the top of this file.
-    // AFMS.begin();
+    AFMS.begin();
 }
 
 void loop(){
