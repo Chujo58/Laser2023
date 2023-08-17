10ee4e3f03f5c98423917fc1159a5b72212f610a
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2023-08-17 16:44:45 -0400

    Added the Adafruit Library

---
+++ laser2023/laser2023.ino
@@ -0,0 +1,32 @@
+#include "Lib/Adafruit_Motor_Shield_V2_Library/Adafruit_MotorShield.h"
+// #include "Adafruit_MotorShield.h"
+#define BAUD_RATE 115200
+
+// #define ADDRESS 0x62
+
+// Here we will define all the of global scope variables.
+
+unsigned int counter = 0;
+int mode = 0;
+unsigned steps = 1000;
+unsigned delays = 20;
+unsigned value = 0;
+
+// Here we call the motor shield which will connect us to the stepper motor connected.
+auto AFMS = Adafruit_MotorShield();
+
+// Here we connect to the stepper motor and we specify that we are using 200 steps per revolution.
+// Adafruit_StepperMotor * myMotor = AFMS.getStepper(200, 1);
+
+void setup(){
+    // To communicate with Python we will do so via the Serial monitor. So we will first initialize it.
+    Serial.begin(BAUD_RATE);
+    Serial.println("LASER 2023");
+
+    // Let's start the motor shield with the default frequency of 1.6kHz. The frequency can always be changed at the top of this file.
+    // AFMS.begin();
+}
+
+void loop(){
+    
+}
